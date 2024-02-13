7e95368a1edf35682f87af6071262d551d0ca833
samuelfr98 <46605132+samuelfr98@users.noreply.github.com>
2024-02-13 15:53:14 -0500

    First commit

---
+++ SloperApp/SloperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SloperAppApp.swift
+//  SloperApp
+//
+//  Created by Sam Friedman on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SloperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
